eef5f844a5fd7aa72966393115eee1def2b603ac
jgravano <juangravano@gmail.com>
2023-09-07 16:38:21 -0300

    updated color palette and added example texts

--- src/components/card.js
+++ src/components/card.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Card({ className, height, width, title, text, imageSrc, link, children }) {
   return (
-    <div className={`card ${className}`} style={{ height: height, width: width, padding: '10px', boxSizing: 'content-box', borderRadius: '15px', overflow: 'hidden' }}>
-      {imageSrc && <img src={imageSrc} alt={title} style={{ width: '100%', borderRadius: '10px' }} />}
-      {title && <h2>{title}</h2>}
-      {text && <p>{text}</p>}
-      {link && <a href={link} target="_blank" rel="noopener noreferrer">Más información</a>}
-      {children}
+    <div className={`card ${className}`} style={{ height: height, width: width, padding: '10px', boxSizing: 'content-box', borderRadius: '15px', overflow: 'hidden', position:'relative' }}>
+      <div className="card-content">
+        {imageSrc && <img src={imageSrc} alt={title} style={{ width: '100%', borderRadius: '10px' }} />}
+        {title && <h2>{title}</h2>}
+        {text && <p>{text}</p>}
+        {link && <a href={link} target="_blank" rel="noopener noreferrer">Más información</a>}
+        {children}
+      </div>
     </div>
   );
 }
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -3,31 +3,36 @@ import Card from './card';
 
 function LandingPage() {
   return (
-    <div style={{ 
-      display: 'flex', 
-      flexDirection: 'column', 
-      height: '100vh', 
-      justifyContent: 'space-between', 
-      background: 'white', 
-      margin: '10px', 
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      justifyContent: 'space-between',
+      background: 'white',
+      margin: '10px',
       padding: '10px',
-      boxSizing: 'content-box'
+      boxSizing: 'border-box', // Cambio de content-box a border-box
+      position: 'relative',
     }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1vw', marginBottom: '10px' }}>
-        <Card className="color1" height="30vh" width="60vw" />
-        <Card className="color2" height="30vh" width="40vw" />
+      <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1vw', marginBottom: '1vh' }}>
+        <Card className="color1" height="28vh" width="60vw" title={"Juan Gravano"} text={"software developer"} imageSrc={""} />
+        <Card className="color2" height="28vh" width="40vw" title={"based in Buenos Aires, Argentina"} />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1vw', marginBottom: '10px' }}>
-        <Card className="color3" height="30vh" width="20vw" />
-        <Card className="color4" height="30vh" width="45vw" />
-        <Card className="color5" height="30vh" width="30vw" />
+      <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1vw', marginBottom: '1vh' }}>
+        <Card className="color3" height="28vh" width="40vw" />
+        <Card className="color4" height="28vh" width="45vw">
+          <div className="color4-text1">making stuff</div>
+          <div className="color4-text2">happen :)</div>
+        </Card>
+        <Card className="color5" height="28vh" width="30vw" />
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-between', gap: '1vw' }}>
-        <Card className="color6" height="30vh" width="50vw" />
-        <Card className="color7" height="30vh" width="50vw" />
+        <Card className="color6" height="28vh" width="20vw" />
+        <Card className="color7" height="28vh" width="80vw" title={"Contact me: "} />
       </div>
     </div>
   );
 }
+
 
 export default LandingPage;

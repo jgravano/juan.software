cf8ff5be955fdd727a3443a28d2629021e0a2f70
jgravano <juangravano@gmail.com>
2024-03-15 18:28:13 -0300

    update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,89 +1,48 @@
 
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import useScroll from '../hooks/useScroll';
+import React, { useRef } from 'react';
 import Card from '../components/card';
+import Header from '../components/header';
 import '../css/card.css';
 import '../css/color1.css';
 import '../css/color2.css';
 import '../css/color3.css';
 import '../css/color4.css';
-import '../css/color5.css';
-import '../css/color6.css';
-import '../css/color7.css';
 import '../css/home.css';
-import NotFoundPage from './404';
+
 
 function LandingPage() {
-  const [showError, setShowError] = useState(false);
-  const circleClasses = ['color1', 'color2', 'color3', 'color4', 'color5', 'color7'];
-  const navigate = useNavigate();
+  const rowRef = useRef(null);
+  const { scrollLeft, scrollRight } = useScroll(rowRef);
 
-
-  const handleDownloadClick = (e) => {
-    e.preventDefault();
-    setShowError(true);
-  }
-
-  if (showError) {
-    navigate("/404");
-    return <NotFoundPage />;
-  }
+  const logos = [
+    { type: 'svg', src: '/images/javascript.svg' },
+    { type: 'svg', src: '/images/kotlin-icon.svg' },
+    { type: 'svg', src: '/images/linux-tux.svg' },
+    { type: 'svg', src: '/images/nodejs.svg' },
+    { type: 'svg', src: '/images/python.svg' },
+    { type: 'svg', src: '/images/react.svg' },
+    { type: 'svg', src: '/images/selenium.svg' },
+    { type: 'svg', src: '/images/webdriverio.svg' },
+  ];
 
   return (
-    <div className="landingPage">
-      <div className="row">
-        <Card className="color1" height="28vh" width="70vw" title={
-          <div className="card-content">
-            <h2>Juan Gravano</h2>
-            <p>software dev</p>
-          </div>
-        }
-          imageSrc="https://docs.google.com/uc?export=download&id=1Dpwel7xUS-bxBfNQ2B_yLvcnDILPox4o" />
-
-        <Card className="color2" height="28vh" width="25vw">
-          <div className="sub-cards-container">
-            <div className="sub-card color-sub1">🇦🇷 Based in Buenos Aires, Argentina.</div>
-            <div className="sub-card color-sub2">🇦🇷 Based in Buenos Aires, Argentina.</div>
-            <div className="sub-card color-sub3">🇦🇷 Based in Buenos Aires, Argentina.</div>
-          </div>
-        </Card>
-      </div>
-
-      <div className="row">
-        <Card className="color3" height="28vh" width="40vw">
-          <a href="path/to/your/CV.pdf" onClick={handleDownloadClick} className="download-link">
-            <h2>download resume<i className="fa fa-chevron-down" style={{ marginLeft: '10px' }}></i></h2>
-          </a>
-        </Card>
-        <Card className="color4" height="28vh" width="40vw">
-          <div className="color4-text1">making stuff happen :)</div>
-        </Card>
-        <Card className="color5 special-card" height="28vh" width="30vw" imageSrc="https://docs.google.com/uc?export=download&id=18wbJL_9dBdYs19hz9Aie5ug_0mKfMW2o" />
-      </div>
-      <div className="row">
-        <Card className="color6" height="28vh" width="25vw">
-          <div className="circle-container">
-            {circleClasses.map((className, index) => (
-              <div key={index} className={`circle ${className}`}></div>
-            ))}
-          </div>
-        </Card>
-        <Card className="color7" height="28vh" width="70vw">
-          <div className="color7-content">
-            <div className="color7-column">
-              <div className="color7-header">
-                <h2>contact me</h2>
-                <div className="color7-icons">
-                  <a href="https://www.github.com/jgravano" target="_blank" rel="noopener noreferrer" className="color7-icon">
-                    <i className="fa fa-github-alt fa-2x"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Card>
-      </div>
+    <>
+    <Header scrollLeft={scrollLeft} scrollRight={scrollRight} />
+    <div className="row card-container">
+      <Card className="color1">
+      </Card>
+      <Card className="color2">
+        <p></p>
+      </Card>
+      <Card className="color3">
+        <p></p>
+      </Card>
+      <Card className="color4">
+        <p></p>
+      </Card>
     </div>
+    </>
   );
 }
 

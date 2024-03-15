3db67e4110f17e4060659e36389ca9e82daa1cf1
jgravano <juangravano@gmail.com>
2024-03-15 18:27:47 -0300

    create header component

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/header.css'; 
+const Header = ({ scrollLeft, scrollRight }) => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <span>Connect your wallet</span>
+      </div>
+      <div className="header-arrows">
+        <button onClick={scrollLeft}>&lt;</button>
+        <button onClick={scrollRight}>&gt;</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

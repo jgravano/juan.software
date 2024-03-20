44f96ae401156f8b118b70a73ddc0e28138d66a8
jgravano <juangravano@gmail.com>
2024-03-20 18:52:46 -0300

    update card component logic

--- src/components/card.js
+++ src/components/card.js
@@ -6,22 +6,26 @@ function Card({ className, title, description, media, children }) {
   return (
     <Tilt className="parallax-effect-glare-scale" perspective={20000} glareEnable={true} glareMaxOpacity={0.25} scale={1.02}>
       <div className={`card ${className}`}>
-          <div className="card-content">
-            {title && <h2>{title}</h2>}
-            {description && <p>{description}</p>}
-              <div className="media-container">
-                {media && media.map((item, index) => (
-                  <React.Fragment key={index}>
-                    {item.type === 'image' && <img src={item.src} alt={`media-${index}`} className="card-image" />}
-                    {item.type === 'svg' && (
-                      typeof item.src === 'string' ? <img src={item.src} alt={`svg-${index}`} className="svg-image" /> : <item.src/>
-                    )}
-                  </React.Fragment>
-                ))}
-              </div>
-            {children}
+        <div className="card-content">
+          {title && <h2>{title}</h2>}
+          {description && <p>{description}</p>}
+          <div className="media-container">
+            {media && media.map((item, index) => (
+              <React.Fragment key={index}>
+                {(item.type === 'image' || item.type === 'svg') && (
+                  <img
+                    src={item.src}
+                    alt={`media-${index}`}
+                    className={item.type === 'svg' ? 'svg-image' : 'card-image'}
+                    style={item.style}
+                  />
+                )}
+              </React.Fragment>
+            ))}
           </div>
+          {children}
         </div>
+      </div>
     </Tilt>
   );
 }
